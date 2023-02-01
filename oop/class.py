d55237329ce4e64a95bb8d9b05c549ef827d027b
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-02 01:58:10 +0200

    add method in class

--- oop/class.py
+++ oop/class.py
@@ -2,8 +2,16 @@ class Cake:
     def __init__(self, p_color):
         self.color = p_color
 
+    def changColor(self, newColor):
+        self.color = newColor
+
+# close class
+
 
 cake1 = Cake("Red")
 cake2 = Cake("Green")
 
 print(cake1.color, cake2.color)
+
+cake1.changColor("Yallo")
+print(cake1.color)
