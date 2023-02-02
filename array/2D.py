52676d07a6cd0c669c38b288b9d426588d55471a
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-02 02:53:47 +0200

    two Dimensions Arrays

---
+++ array/2D.py
@@ -0,0 +1,9 @@
+arr = [[1, 2, 3], [4, 5, 6]]
+
+# to print the first column
+for i in range(len(arr[0])):
+    print(arr[0][i], end=" ")
+
+# to print the second column
+for i in range(len(arr[1])):
+    print(arr[1][i], end=" ")
