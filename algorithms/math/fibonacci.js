1dc8c8bf815604f0b9bbbc156af70d0cc6fdf314
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-08 03:16:07 +0200

    update

--- algorithms/math/fibonacci.js
+++ algorithms/math/fibonacci.js
@@ -1,17 +1,18 @@
-// function fibonacciSequence(n) {
-//     let fibonacci = [0, 1];
-//     if (n > 2) {
-//         for (let i = 0; i < n-2; i++) {
-//             let newNumber = fibonacci[i] + fibonacci[i + 1];
-//             fibonacci.push(newNumber);
-//         }
-//     }
-//     return fibonacci
-// }
+function fibonacciSequence1(n) {
+    let fibonacci = [0, 1];
+    if (n > 2) {
+        for (let i = 0; i < n-2; i++) {
+            let newNumber = fibonacci[i] + fibonacci[i + 1];
+            fibonacci.push(newNumber);
+        }
+    }
+    return fibonacci
+}
 
 
 
-// BIG-O = O(n)function fibonacciSequence(n) {
+// BIG-O = O(n)
+function fibonacciSequence2(n) {
     let fib = [0, 1];
     for (let i = 2; i < n; i++) {
         fib[i] = fib[i - 1] + fib[i - 2]
@@ -19,6 +20,6 @@
     return fib
 }
 
-console.log(fibonacciSequence(2));
-console.log(fibonacciSequence(3));
-console.log(fibonacciSequence(7));
+console.log(fibonacciSequence2(2));
+console.log(fibonacciSequence2(3));
+console.log(fibonacciSequence2(7));
