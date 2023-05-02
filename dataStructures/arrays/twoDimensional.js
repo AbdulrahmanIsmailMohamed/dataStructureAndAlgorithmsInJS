f8e01f5313b73f897af81cbedbf70197d6aca9ed
AbdulrahmanIsmailMohamed <abdulrahman.ismail.mohammed@gmail.com>
2023-05-02 09:47:19 +0300

    Two Dimensional Array Or Matrix

---
+++ dataStructures/arrays/twoDimensional.js
@@ -0,0 +1,10 @@
+// Each row is represented by a separate list within the outer list
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// To access individual elements of the matrix, you use the row and column indices like this:
+const element = matrix[1][2] 
+console.log(element); // 6
